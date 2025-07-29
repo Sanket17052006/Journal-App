f501cce943c60618f1ff94bd0c6728c354146ee4
Sanket Srivastava <sanket17052006@gmail.com>
2025-07-29 22:32:21 +0530

    Add GETONE, PUT, DELETE endpoints to JournalEntryController

--- src/main/java/com/sanket/journalApp/controller/JournalEntryController.java
+++ src/main/java/com/sanket/journalApp/controller/JournalEntryController.java
@@ -1,6 +1,7 @@
 package com.sanket.journalApp.controller;
 
 import com.sanket.journalApp.entity.JournalEntry;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -20,10 +21,42 @@ public class JournalEntryController {
         return new ArrayList<>(journalEntries.values());
     }
 
+    @GetMapping("/id/{myId}")
+    public ResponseEntity<JournalEntry> getOne(@PathVariable Long myId) {
+        JournalEntry entry = journalEntries.get(myId);
+        if (entry == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(entry);
+    }
+
+
     @PostMapping()
     public boolean createEntry(@RequestBody JournalEntry myEntry){// localhost:8080/journal POST
         journalEntries.put(myEntry.getId(), myEntry);
         return true;
 
+
     }
+
+    @PutMapping("/id/{myId}")
+    public ResponseEntity<String> modify(@PathVariable Long myId, @RequestBody JournalEntry myEntry) {
+        if (!journalEntries.containsKey(myId)) {
+            return ResponseEntity.notFound().build();
+        }
+        journalEntries.put(myId, myEntry);
+        return ResponseEntity.ok("Entry updated");
+    }
+
+    @DeleteMapping("/id/{myId}")
+    public ResponseEntity<String> delete(@PathVariable Long myId,@RequestBody JournalEntry myEntry){
+        if (!journalEntries.containsKey(myId)) {
+            return ResponseEntity.notFound().build();
+        }
+        journalEntries.remove(myId);
+        return ResponseEntity.ok("Entry deleted");
+
+    }
+
+
 }
